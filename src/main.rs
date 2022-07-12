e9596010abce1d1973d1c49087139a5d043ab684
Sumeet Naik <25149022+sumeetweb@users.noreply.github.com>
2022-07-12 10:46:40 +0000

    Hello World

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,4 @@
-// Make it work, don't modify `implicitly_ret_unit` !
 fn main() {
-    let _v: () = ();
-
-    let v = ();
-    assert_eq!(v, implicitly_ret_unit());
-
-    println!("Success!");
+    println!("Hello world. This repo is for learning Rust.");
 }
 
-fn implicitly_ret_unit() {
-    println!("I will return a ()");
-}
-
-// Don't use this one
-fn explicitly_ret_unit() -> () {
-    println!("I will return a ()");
-}
